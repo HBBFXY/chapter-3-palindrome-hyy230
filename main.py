a7c56a19d2472146719f57e3e0561bba941f459d
hyy230 <2638936581@qq.com>
2025-09-13 17:38:24 +0800

    Update main.py
    作业

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 def main():
     num_str=input().strip()
+    
     if not num_str.isdigit():
         print("错误：输入必须为纯数字!")
         return
+        
     if len(num_str)!=5:
-        print("错误：输入必须为五位数!")
+        print("错误：输入必须为5位数!")
         return
+        
     if num_str==num_str[::-1]:
-        print("这是一个回文数")
+        print("是回文数")
     else:
-        print("这不是一个回文数")
+        print("不是回文数")
 if __name__=="__main__":
     main()
